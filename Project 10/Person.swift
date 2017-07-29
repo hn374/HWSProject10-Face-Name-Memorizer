82c6cef8d653ae1bbe0cd1df9c42ecdf0cf10182
Hoang Nguyen <mizore742@hotmail.com>
2017-07-28 23:48:51 -0400

    Initial commit.

---
+++ Project 10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project 10
+//
+//  Created by Hoang Nguyen on 7/28/17.
+//  Copyright © 2017 Hoang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project 10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Hoang Nguyen on 7/28/17.
+//  Copyright © 2017 Hoang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel! 
+    
+}
